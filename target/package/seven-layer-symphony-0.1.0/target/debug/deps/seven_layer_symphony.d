9592b6a8bf613be93f0dc174d3c6b83fec73772e
s0fractal <sergey.glova@gmail.com>
2025-08-31 20:40:58 +0300

    🔧 Fix panic settings for no_std

---
+++ target/package/seven-layer-symphony-0.1.0/target/debug/deps/seven_layer_symphony.d
@@ -0,0 +1,7 @@
+/Users/chaoshex/Projects/seven-layer-symphony/target/package/seven-layer-symphony-0.1.0/target/debug/deps/seven_layer_symphony.d: src/lib.rs
+
+/Users/chaoshex/Projects/seven-layer-symphony/target/package/seven-layer-symphony-0.1.0/target/debug/deps/libseven_layer_symphony.dylib: src/lib.rs
+
+/Users/chaoshex/Projects/seven-layer-symphony/target/package/seven-layer-symphony-0.1.0/target/debug/deps/libseven_layer_symphony.rlib: src/lib.rs
+
+src/lib.rs:
---
+++ target/package/seven-layer-symphony-0.1.0/target/wasm32-unknown-unknown/release/deps/seven_layer_symphony.d
@@ -0,0 +1,7 @@
+/Users/chaoshex/Projects/seven-layer-symphony/target/wasm32-unknown-unknown/release/deps/seven_layer_symphony.d: src/lib.rs
+
+/Users/chaoshex/Projects/seven-layer-symphony/target/wasm32-unknown-unknown/release/deps/seven_layer_symphony.wasm: src/lib.rs
+
+/Users/chaoshex/Projects/seven-layer-symphony/target/wasm32-unknown-unknown/release/deps/libseven_layer_symphony.rlib: src/lib.rs
+
+src/lib.rs:
